3f584893c757f05bae563774b0e6e5280bd82d59
Misha Tuesday <mishatuesday@gmail.com>
2022-10-20 10:22:12 -0400

    initial commit, display all plants working

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
+const dataUrl = 'http://localhost:6001/plants/'
 
 function PlantPage() {
+  const [plantData, setPlantData] = useState([])
+
+  useEffect(() => {
+    fetch(dataUrl)
+    .then(r => r.json())
+    .then(data => setPlantData(data))
+  }, [])
+
+  function addNewPlant(plant) {
+    console.log("adding new plant", plant)
+    // POST to db.json
+
+    // use response.id to update plantData
+  }
+
   return (
     <main>
-      <NewPlantForm />
+      <NewPlantForm addNewPlant={addNewPlant} />
       <Search />
-      <PlantList />
+      <PlantList plantData={plantData} />
     </main>
   );
 }
